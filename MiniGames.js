9d7443a4b32deaf4588fe137d84ad4384cfad9ef
JjoWeb <134334959+JjoWeb@users.noreply.github.com>
2023-09-22 14:21:27 -0600

    Update MiniGames.js
    - Added JavaScript code for Video Quiz game
    - Other small corrections

--- MiniGames.js
+++ MiniGames.js
@@ -88,11 +88,11 @@ document.addEventListener('DOMContentLoaded', (event) => {
 });
 
 const imagesBank = [
-   "docs/assets/anger.jpg",
-   "docs/assets/happiness.jpg",
-   "docs/assets/sadness.jpg",
-   "docs/assets/envy.jpg",
-   "docs/assets/worry.jpg",
+   "images/anger.jpg",
+   "images/happiness.jpg",
+   "images/sadness.jpg",
+   "images/envy.jpg",
+   "images/worry.jpg",
 ];
 const answersBank = [
    "anger",
@@ -166,3 +166,144 @@ function reviewAnswer() {
       result.innerHTML = "Nope! Try again...";
    }
 }
+
+
+//VIDEO QUIZ SECTION
+document.addEventListener('DOMContentLoaded', (event) => {
+
+   const video = document.querySelector('#myVideo');
+   const questionOneSection = document.querySelector('#questionOneSection');
+   const q1Check1 = document.querySelector('#q1Check1');
+   const q1Check2 = document.querySelector('#q1Check2');
+   const q1Result = document.querySelector('#q1Result');
+   const q1resumeButton = document.querySelector('#q1resumeButton');
+   const questionTwoSection = document.querySelector('#questionTwoSection');
+   const q2Check1 = document.querySelector('#q2Check1');
+   const q2Check2 = document.querySelector('#q2Check2');
+   const q2Result = document.querySelector('#q2Result');
+   const q2resumeButton = document.querySelector('#q2resumeButton');
+   const questionThreeSection = document.querySelector('#questionThreeSection');
+   const q3Check1 = document.querySelector('#q3Check1');
+   const q3Check2 = document.querySelector('#q3Check2');
+   const q3Result = document.querySelector('#q3Result');
+   const finalScore = this.document.querySelector('#finalScore');
+   const q3Button = document.querySelector('#q3Button');
+   let i = 0;
+
+   function showQuestions() {
+       if ((video.currentTime >= 5) && (video.currentTime <= 5.9)) {
+           video.pause();
+           video.style.display = 'none';
+           questionOneSection.style.display = 'block';
+           q1Result.innerHTML = "";
+       } else if 
+           ((video.currentTime >= 12) && (video.currentTime <= 12.9)) {
+           video.pause();
+           video.style.display = 'none';
+           questionTwoSection.style.display = 'block';
+           q2Result.innerHTML = "";
+       } else if 
+           (video.currentTime >= 19) {
+           video.pause();
+           video.style.display = 'none';
+           questionThreeSection.style.display = 'block';
+           q3Result.innerHTML = "";
+           finalScore.innerHTML = "";
+       };
+   };
+
+
+   function correct1() {
+       q1Check1.setAttribute('disabled', '');
+       q1Check2.setAttribute('disabled', '');
+       q1Result.innerHTML = "CORRECT!";
+       i++;
+       q1resumeButton.style.display = 'block';
+   };
+
+   function incorrect1() {
+       q1Check1.setAttribute('disabled', '');
+       q1Check2.setAttribute('disabled', '');
+       q1Result.innerHTML = "INCORRECT!";
+       q1resumeButton.style.display = 'block';
+   };
+   
+   function q1resumeVideo() {
+       questionOneSection.style.display = 'none';
+       video.style.display = 'block';
+       video.currentTime = 6;
+       video.play();
+   };
+
+
+   function correct2() {
+       q2Check1.setAttribute('disabled', '');
+       q2Check2.setAttribute('disabled', '');
+       q2Result.innerHTML = "CORRECT!";
+       i++;
+       q2resumeButton.style.display = 'block';
+   };
+
+   function incorrect2() {
+       q2Check1.setAttribute('disabled', '');
+       q2Check2.setAttribute('disabled', '');
+       q2Result.innerHTML = "INCORRECT!";
+       q2resumeButton.style.display = 'block';
+   };
+
+   function q2resumeVideo() {
+       questionTwoSection.style.display = 'none';
+       video.style.display = 'block';
+       video.currentTime = 13;
+       video.play();
+   };
+
+   
+   function correct3() {
+       q3Check1.setAttribute('disabled', '');
+       q3Check2.setAttribute('disabled', '');
+       q3Result.innerHTML = "CORRECT!";
+       i++;
+       finalScore.innerHTML = "Your score is " +  i + "/3";
+       q3Button.style.display = 'block';
+   };
+
+   function incorrect3() {
+       q3Check1.setAttribute('disabled', '');
+       q3Check2.setAttribute('disabled', '');
+       q3Result.innerHTML = "INCORRECT!";
+       finalScore.innerHTML = "Your score is " +  i + "/3";
+       q3Button.style.display = 'block';
+   };
+
+
+   function playAgain() {
+       q1Check1.removeAttribute('disabled', '');
+       q1Check2.removeAttribute('disabled', '');
+       q2Check1.removeAttribute('disabled', '');
+       q2Check2.removeAttribute('disabled', '');
+       q3Check1.removeAttribute('disabled', '');
+       q3Check2.removeAttribute('disabled', '');
+       q1resumeButton.style.display = 'none';
+       q2resumeButton.style.display = 'none';
+       q3Button.style.display = 'none';
+       i = 0;
+       questionThreeSection.style.display = 'none';
+       video.style.display = 'block';
+       video.currentTime = 0;
+       video.play();
+   };
+
+
+   video.addEventListener('timeupdate', showQuestions);
+   q1Check1.addEventListener('click', correct1);
+   q1Check2.addEventListener('click', incorrect1);
+   q2Check1.addEventListener('click', correct2);
+   q2Check2.addEventListener('click', incorrect2);
+   q3Check1.addEventListener('click', correct3);
+   q3Check2.addEventListener('click', incorrect3);
+   q1resumeButton.addEventListener('click', q1resumeVideo);
+   q2resumeButton.addEventListener('click', q2resumeVideo);
+   q3Button.addEventListener('click', playAgain);
+
+});
